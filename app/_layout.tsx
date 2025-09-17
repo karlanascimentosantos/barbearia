9ebd420686923658008f1454d3a8d921fd76b8f9
karla <karla.mikaelly@academico.ifpb.edu.br>
2025-09-17 19:59:24 -0300

    Problemas no expo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,25 @@
-import { Stack } from "expo-router";
+import { Slot } from 'expo-router'; // Slot renderiza as páginas filhos
+import { AuthProvider } from '../context/AuthContext';
+import { useFonts } from 'expo-font';
+import { View, Text } from 'react-native';
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    OvoRegular: require('../assets/fonts/Ovo-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    // Enquanto a fonte carrega, podemos mostrar um placeholder simples
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Carregando...</Text>
+      </View>
+    );
+  }
+
   return (
-  <Stack>
-
-    <Stack.Screen
-     name="index" 
-     options={{ title: "HOME"}}/>
-
-    </Stack>
+    <AuthProvider>
+      <Slot /> {/* Renderiza todas as páginas da pasta */}
+    </AuthProvider>
   );
 }
